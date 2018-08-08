6045cd06d532b067e54598422e2698e1dfd3f042
Glitch (elated-hero) <none>
2018-08-08 02:14:45 +0000

    😄🌴 Updated with Glitch

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+/*
+*
+*
+*       Complete the API routing below
+*
+*
+*/
+
+'use strict';
+
+var expect = require('chai').expect;
+var ConvertHandler = require('../controllers/convertHandler.js');
+
+module.exports = function (app) {
+  
+  var convertHandler = new ConvertHandler();
+
+  app.route('/api/convert')
+    .get(function (req, res){
+      var input = req.query.input;
+      var initNum = convertHandler.getNum(input);
+      var initUnit = convertHandler.getUnit(input);
+      var returnNum = convertHandler.convert(initNum, initUnit);
+      var returnUnit = convertHandler.getReturnUnit(initUnit);
+      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      
+      if (returnUnit === 'invalid unit' && (initNum === 'invalid number' || returnNum === 'invalid number')) {res.send('invalid number and unit');}
+      else if (returnUnit === 'invalid unit') {res.send('invalid unit');}
+      else if (initNum === 'invalid number' || returnNum === 'invalid number') {res.send('invalid number');}
+      else {var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+        res.json({initNum: initNum, initUnit: initUnit,
+                returnNum: returnNum, returnUnit: returnUnit, string: toString});
+        }
+      });
+      //res.json
+    
+};
